9ca539d106c39e47180935c14b5b6cd8a2f494c1
Pavel <Grinchenko>
2023-01-27 16:11:57 +0200

    :bug: Fixed bugs with scroll to anchors

---
+++ src/js/scroll.js
@@ -0,0 +1,21 @@
+const matches = document.querySelectorAll("p");
+
+document.querySelectorAll('a[href^="#"').forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    let href = this.getAttribute("href").substring(1);
+
+    const scrollTarget = document.getElementById(href);
+
+    const topOffset = document.querySelector(".header").offsetHeight;
+    // const topOffset = 77; // если не нужен отступ сверху
+    const elementPosition = scrollTarget.getBoundingClientRect().top;
+    const offsetPosition = elementPosition - topOffset;
+
+    window.scrollBy({
+      top: offsetPosition,
+      behavior: "smooth",
+    });
+  });
+});

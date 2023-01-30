a535aa38f073fcb8d05b66563b5ea901ae3866d9
Pavel <Grinchenko>
2023-01-30 10:25:13 +0200

    :bug: Fixed bugs and added favicon

--- src/js/email.js
+++ src/js/email.js
@@ -5,21 +5,50 @@ function sendEmail() {
   let whatsNeed = document.getElementById("whatsNeed").value;
   let learnAboutUs = document.getElementById("learnAboutUs").value;
 
-  Email.send({
-    Host: "smtp.elasticemail.com",
-    Username: "pablooeescobaro@gmail.com",
-    Password: "B8D997D8EE74481CC40C0BC15C0F5A9A20F7",
-    To: "pablooeescobaro@gmail.com",
-    From: "kvartalshow@gmail.com",
-    Subject: "Order",
-    Body: `
-    Name: ${name} \n 
-    Email: ${email} \n 
-    Number: ${number} \n 
-    How can we help: ${whatsNeed} \n 
-    How did you learn about us?: ${learnAboutUs}
-    `,
-  }).then(function (message) {
-    alert(message);
-  });
+  let errorLine = document.getElementById("inputError");
+  let loadingButtom = document.getElementById("loadingButton");
+
+  if (name === "") {
+    errorLine.innerHTML = "The name field cannot be empty!";
+  } else if (name.length < 2) {
+    errorLine.innerHTML = "The name cannot be less than 2 letters!";
+  } else if (!validateEmail(email)) {
+    errorLine.innerHTML = "Invalid email address!";
+  } else if (whatsNeed === "") {
+    errorLine.innerHTML = "Be sure to let me know how I can help.";
+  } else {
+    loadingButtom.classList.add("button--sending");
+    Email.send({
+      Host: "smtp.elasticemail.com",
+      Username: "pablooeescobaro@gmail.com",
+      Password: "B8D997D8EE74481CC40C0BC15C0F5A9A20F7",
+      To: "pablooeescobaro@gmail.com",
+      From: "kvartalshow@gmail.com",
+      Subject: "Order",
+      Body: `
+      Name: ${name} \n
+      Email: ${email} \n
+      Number: ${number} \n
+      How can we help: ${whatsNeed} \n
+      How did you learn about us?: ${learnAboutUs}
+      `,
+    }).then(function (message) {
+      if (message == "OK") {
+        errorLine.classList.add("input__error--green")
+        errorLine.innerHTML = "Message sent"
+      }
+      loadingButtom.classList.remove("button--sending");
+    });
+  }
+}
+
+function validateEmail(email) {
+  var validRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  if (email.match(validRegex)) {
+    return true;
+  } else {
+    return false;
+  }
 }
